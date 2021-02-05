97c1972d64c31d5e81efd8e9ec53d5c3abfa5d93
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 09:36:07 -0600

    Add CORS Headers to server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,13 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 /* EXPRESS SETUP END */
 
 /* MONGOOSE SETUP */
