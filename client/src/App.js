edd9848d5bcf0ab2c17af4e5d3296d87844a5cde
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 07:34:37 -0600

    Add basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        {/* <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+          </Switch> */}
+      </div>
+    </Router>
+  );
 }
