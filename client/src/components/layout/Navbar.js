a1dade4aac6831f74a652f9db3754245d55a536b
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 09:56:45 -0600

    Make title bigger

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className="flex justify-center py-4">
       <nav className="">
-        <Link to="/" className="text-xl text-gray-800">
+        <Link to="/" className="text-2xl font-bold text-gray-800">
           Auth App
         </Link>
       </nav>
