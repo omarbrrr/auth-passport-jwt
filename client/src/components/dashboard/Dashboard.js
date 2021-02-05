ef536da168241c87682a3500719e236ca5dbc6fb
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 08:17:36 -0600

    Update Dashboard component, added Redux logout action

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-export default function Dashboard() {
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+
+function Dashboard(props) {
+  const { user } = props.auth;
+
   const onLogoutClick = (e) => {
     e.preventDefault();
+    props.logoutUser();
   };
 
   return (
     <div className="w-full mt-20 text-center">
+      <p className="mb-4 text-xl">Hello, {user.name.split(" ")[0]}</p>
       <p className="mb-12 text-lg">You are currently logged in 👏</p>
       <button
         onClick={onLogoutClick}
@@ -17,3 +25,14 @@ export default function Dashboard() {
     </div>
   );
 }
+
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
