787b2786f4b89d5807e44d4ae2ae75b4a69b7cf3
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 07:33:09 -0600

    Add simple navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center py-4">
+      <nav className="">
+        <Link to="/" className="text-xl text-gray-800">
+          Auth App
+        </Link>
+      </nav>
+    </div>
+  );
+}
