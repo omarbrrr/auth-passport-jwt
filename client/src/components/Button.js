791dbb9911ac8cdec69c35d95261f36f6e41597a
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-08 14:25:11 -0600

    Create reusable Button component

---
+++ client/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Button({ type, classNames = "", children }) {
+  const btnClassNames = `block mx-auto py-2 px-8 rounded-lg text-gray-100 bg-indigo-600 ${classNames}`;
+
+  return (
+    <button type={type} className={btnClassNames}>
+      {children}
+    </button>
+  );
+}
