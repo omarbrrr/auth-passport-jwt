0dd5fe681e1ba9c995c5f4c983e9a61628a539c4
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-08 14:31:32 -0600

    Add a reusable container for auth screens

---
+++ client/src/components/auth/AuthContainer.js
@@ -0,0 +1,7 @@
+export default function AuthContainer({ children }) {
+  return (
+    <div className="max-w-xl w-5/6 mx-auto mt-12 py-4 px-6 bg-white rounded-lg shadow-md">
+      {children}
+    </div>
+  );
+}
