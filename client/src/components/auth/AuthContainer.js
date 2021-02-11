e7fec9ccf0b64d26ca6b1b3b44a1cc7ce3cc86a7
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-10 19:32:29 -0600

    Increase shadow box

--- client/src/components/auth/AuthContainer.js
+++ client/src/components/auth/AuthContainer.js
@@ -1,6 +1,6 @@
 export default function AuthContainer({ children }) {
   return (
-    <div className="max-w-xl w-5/6 mx-auto mt-12 py-4 px-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-xl w-5/6 mx-auto mt-12 py-4 px-6 bg-white rounded-lg shadow-lg">
       {children}
     </div>
   );
