3101829682dd31f98a72d6bbfd92379bd0dadf1a
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-10 19:42:52 -0600

    Add a hover state

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -7,7 +7,7 @@ export default function Button({
   disabled,
   children,
 }) {
-  const btnClassNames = `block mx-auto py-2 px-8 rounded-lg text-gray-100 bg-indigo-600 ${classNames}`;
+  const btnClassNames = `block mx-auto py-2 px-8 rounded-lg text-gray-100 bg-indigo-600 hover:bg-indigo-800 ${classNames}`;
 
   return (
     <button
