109f4e1d5b1aeb8f1ac630c22be4baedfbbb33d9
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-08-15 19:58:28 -0500

    Fix Redux issue on development

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
@@ -8,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
